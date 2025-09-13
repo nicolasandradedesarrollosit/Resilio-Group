b4d5ce338c8ee2dcd74307b1077baa0451997b4c
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-09-13 15:31:58 -0300

    pagina de contacto y landing terminadas

--- src/view/components/home/SectionFeatures.jsx
+++ src/view/components/home/SectionFeatures.jsx
@@ -4,22 +4,30 @@ import '../../../styles/home/sectionFeatures.css';
 function SectionFeatures(){
     const features = [
         {
-            svg:"💳",
+            svg:(
+                <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 512 512"><path fill="#FFF" d="M32 376a56 56 0 0 0 56 56h336a56 56 0 0 0 56-56V222H32Zm66-76a30 30 0 0 1 30-30h48a30 30 0 0 1 30 30v20a30 30 0 0 1-30 30h-48a30 30 0 0 1-30-30ZM424 80H88a56 56 0 0 0-56 56v26h448v-26a56 56 0 0 0-56-56Z"/></svg>
+            ),
             name:"Descuentos exclusivos",
             content:"Accedé a precios preferenciales en marcas asociadas."
         },
         {
-            svg:"⭐",
+            svg:(
+                <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 26 26"><path fill="#FFF" d="M25.326 10.137a1.001 1.001 0 0 0-.807-.68l-7.34-1.066l-3.283-6.651c-.337-.683-1.456-.683-1.793 0L8.82 8.391L1.48 9.457a1 1 0 0 0-.554 1.705l5.312 5.178l-1.254 7.31a1.001 1.001 0 0 0 1.451 1.054L13 21.252l6.564 3.451a1 1 0 0 0 1.451-1.054l-1.254-7.31l5.312-5.178a.998.998 0 0 0 .253-1.024z"/></svg>
+            ),
             name:"Acumulación de puntos",
             content:"Sumá puntos en cada compra y canjealos por más beneficios."
         },
         {
-            svg:"🚀",
+            svg:(
+                <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24"><path fill="#FFF" d="M22.6 2.1c-.1-.3-.4-.6-.7-.7c-4.2-1.1-8.5.2-11.4 3.6L9.4 6.3l-2.7-.6C5.4 5.2 4 5.8 3.4 7l-2.2 3.9c-.2.3-.2.6 0 .9c.1.3.4.5.7.6l3.1.7c-.3.8-.4 1.6-.6 2.4c0 .3.1.6.3.8l3.1 3.1c.2.2.4.3.7.3h.1c.9-.1 1.7-.2 2.5-.5l.6 3c.1.3.3.6.6.7c.1.1.3.1.4.1c.2 0 .3 0 .5-.1l3.9-2.2c1.1-.6 1.7-2 1.4-3.3l-.7-2.8l1.2-1.1c3.3-2.8 4.7-7.3 3.6-11.4zM7.3 8.8c-.6.8-1.2 1.6-1.6 2.4l-2.1-.5L5.1 8c.2-.4.6-.5 1.1-.4l1.7.4l-.6.8zM16 18.9l-2.7 1.5l-.4-2c.9-.4 1.7-1 2.4-1.6l.7-.7l.4 1.7c.2.5-.1 1-.4 1.1zm.7-10.1c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5s1.5.7 1.5 1.5s-.6 1.5-1.5 1.5z"/></svg>
+            ),
             name:"Acceso prioritario",
             content:"Entrá primero a promociones y lanzamientos limitados."
         },
         {
-            svg:"📱",
+            svg:(
+                <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 8 8"><path fill="#FFF" d="M1.19 0C1.08 0 1 .08 1 .19v7.63c0 .11.08.19.19.19h4.63c.11 0 .19-.08.19-.19V.19c0-.11-.08-.19-.19-.19H1.19zM2 1h3v5H2V1zm1.5 5.5c.28 0 .5.22.5.5s-.22.5-.5.5S3 7.28 3 7s.22-.5.5-.5z"/></svg>
+            ),
             name:"Gestión simple",
             content:"Todo desde una app centralizada, fácil y rápida."
         }
--- src/view/components/home/SectionHowWorks.jsx
+++ src/view/components/home/SectionHowWorks.jsx
@@ -4,17 +4,23 @@ import '../../../styles/home/sectionHowWorks.css'
 function SectionHowWorks(){
     const howWorks = [
     {
-        svg: "📝",
+        svg: (
+            <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 17 16"><path fill="#FFFF" fill-rule="evenodd" d="M15.016 0v1.031h-1.062V0h-.895v1.031h-1.09V0h-.953v1.031H9.954V0h-.922v1.031H7.941V0h-.925v1.031H5.985V0h-.942v1.031H3.959V0H3v16h12.954V0h-.938zM5 6.958h9v1H5v-1zm9 5H5v-1h9v1zM14 10H5V9h9v1z"/></svg>
+        ),
         name: "1. Registrate",
         content: "Creá tu cuenta en pocos pasos y accedé a los planes de nuestra red de beneficios exclusivos."
     },
     {
-        svg: "🤝",
+        svg: (
+            <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24"><path fill="#fff" d="M2.284 22.158c.001.95.679 1.752 1.62 1.792c1.31.055 2.623.022 3.935.022c.954 0 1.786-.865 1.76-1.954c-.029-1.221.018-2.445.029-3.667l.045-4.988c.003-.305.046-.362.335-.44a4.242 4.242 0 0 1 2.013-.067c1.23.262 2.129 1.21 2.261 2.46c.066.62.07 1.249.078 1.874c.025 1.64.038 3.28.054 4.921c.01 1.087.796 1.877 1.883 1.882c1.171.006 2.342.008 3.513.007c1.106-.002 1.895-.778 1.898-1.883c.007-3.371.007-6.742.01-10.113c0-.052 0-.105-.005-.156c-.03-.355-.169-.658-.483-.838a2.638 2.638 0 0 0-.695-.291a7.484 7.484 0 0 0-2.887-.123c-.743.113-1.476.293-2.213.442c-.97.196-1.946.28-2.934.178c-1.268-.129-2.37-.666-3.402-1.38a32.36 32.36 0 0 0-1.494-.984c-.62-.38-1.314-.505-2.03-.544c-.77-.043-1.536-.063-2.293.111c-.59.136-.899.479-.966 1.077a3.438 3.438 0 0 0-.021.379m7.337-6.184a3.675 3.675 0 1 0-7.35-.031a3.675 3.675 0 0 0 7.35.03zm8.335-1.81a3.673 3.673 0 0 0-3.69 3.652a3.672 3.672 0 0 0 3.67 3.697a3.678 3.678 0 0 0 3.68-3.665a3.677 3.677 0 0 0-3.66-3.685Z"/></svg>
+        ),
         name: "2. Elegí tu partner",
         content: "Explorá las marcas asociadas, activá tus beneficios y empezá a ahorrar."
     },
     {
-        svg: "🎉",
+        svg: (
+            <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 48 48"><mask id="ipSFireworks0"><g fill="none"><path fill="#FFFF" stroke="#FFFF" stroke-linejoin="round" stroke-width="4" d="m6 42l8.674-24.736L31 34.038L6 42Z"/><path stroke="#FFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m23 19l5-5c2.667-2.667 3-5 1-7m0 18l5-5c3.333-3.333 6.667-3.333 10 0"/><path fill="#FFFF" d="M20 7a2 2 0 1 0 0-4a2 2 0 0 0 0 4Zm22-1a2 2 0 1 0 0-4a2 2 0 0 0 0 4Zm0 23a2 2 0 1 0 0-4a2 2 0 0 0 0 4Zm-3 9a2 2 0 1 0 0-4a2 2 0 0 0 0 4Z"/></g></mask><path fill="#FFFF" d="M0 0h48v48H0z" mask="url(#ipSFireworks0)"/></svg>
+        ),
         name: "3. Disfrutá las recompensas",
         content: "Acumulá puntos, recibí descuentos y accedé a promociones especiales."
     }
